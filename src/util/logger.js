a5b1a6853b1d0155507e1e8f3404b154f11fb4d7
ribyaboi <moritz.kohl@otto.de>
2018-08-08 19:43:31 +0200

    added log directory creation on module clal

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,19 +1,31 @@
 const winston = require('winston');
+const fs = require('fs');
+const rootPath = require('app-root-path').path;
+const logPath = `${rootPath}/log`;
 
-module.exports = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.File({
-            filename: 'error.log',
-            level: 'error'
-        }),
-        new winston.transports.File({
-            filename: 'bot.log',
-        }),
-        new winston.transports.Console({
-            handleExceptions: true,
-            colorize: true
-        })
-    ]
-});
+function getLogger() {
+    if(!fs.existsSync(logPath)) {
+        fs.mkdirSync(logPath);
+    }
+
+    return winston.createLogger({
+        level: 'info',
+        format: winston.format.json(),
+        transports: [
+            new winston.transports.File({
+                filename: `${logPath}/error.log`,
+                level: 'error'
+            }),
+            new winston.transports.File({
+                filename: `${logPath}/bot.log`,
+            }),
+            new winston.transports.Console({
+                handleExceptions: true,
+                colorize: true,
+                json: false
+            })
+        ]
+    });
+};
+
+module.exports = getLogger();
